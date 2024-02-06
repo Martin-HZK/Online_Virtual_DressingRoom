1728e385520d52ad1f5862a2e817636d7d3e078a
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-02-06 20:58:04 +0800

    Create upload button to open the file browser

---
+++ ReactDev/MovieClient/movie-gold-v1/src/icons/recommendation/RecommendationIcon.js
@@ -0,0 +1,21 @@
+// icon:filetype-ai | Bootstrap https://icons.getbootstrap.com/ | Bootstrap
+import * as React from "react";
+
+function RecommendationIcon(props) {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M14 4.5V14a2 2 0 01-2 2H6v-1h6a1 1 0 001-1V4.5h-2A1.5 1.5 0 019.5 3V1H4a1 1 0 00-1 1v9H2V2a2 2 0 012-2h5.5L14 4.5zM1.113 14.82L.8 15.85H0l1.342-3.999h.926l1.336 3.999h-.841l-.314-1.028H1.113zm1.178-.588l-.49-1.617h-.034l-.49 1.617h1.014zm2.425-2.382v3.999h-.791V11.85h.79z"
+      />
+    </svg>
+  );
+}
+
+export default RecommendationIcon;
---
+++ ReactDev/MovieClient/movie-gold-v1/src/icons/shoppingCart/ShoppingCartIcon.js
@@ -0,0 +1,21 @@
+// icon:shopping-cart | Typicons https://www.s-ings.com/typicons/ | Stephen Hutchings
+import * as React from "react";
+
+function ShoppingCartIcon(props) {
+  return (
+    <svg
+      baseProfile="tiny"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M20.756 5.345A1.003 1.003 0 0020 5H6.181l-.195-1.164A1 1 0 005 3H2.75a1 1 0 100 2h1.403l1.86 11.164.045.124.054.151.12.179.095.112.193.13.112.065a.97.97 0 00.367.075H18a1 1 0 100-2H7.847l-.166-1H19a1 1 0 00.99-.858l1-7a1.002 1.002 0 00-.234-.797zM18.847 7l-.285 2H15V7h3.847zM14 7v2h-3V7h3zm0 3v2h-3v-2h3zm-4-3v2H7l-.148.03L6.514 7H10zm-2.986 3H10v2H7.347l-.333-2zM15 12v-2h3.418l-.285 2H15z" />
+      <path d="M10 19.5 A1.5 1.5 0 0 1 8.5 21 A1.5 1.5 0 0 1 7 19.5 A1.5 1.5 0 0 1 10 19.5 z" />
+      <path d="M19 19.5 A1.5 1.5 0 0 1 17.5 21 A1.5 1.5 0 0 1 16 19.5 A1.5 1.5 0 0 1 19 19.5 z" />
+    </svg>
+  );
+}
+
+export default ShoppingCartIcon;
