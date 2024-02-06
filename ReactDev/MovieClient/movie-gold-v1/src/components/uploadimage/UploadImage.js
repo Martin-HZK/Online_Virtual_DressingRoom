99350126d9e843f76e7fd5e82b1022b2214f0c33
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-02-06 21:04:58 +0800

    Create upload function

--- ReactDev/MovieClient/movie-gold-v1/src/components/uploadimage/UploadImage.js
+++ ReactDev/MovieClient/movie-gold-v1/src/components/uploadimage/UploadImage.js
@@ -1,13 +1,56 @@
+import { Button } from '@mui/material';
+import './UploadImage.css';
+import { useState, useRef } from 'react'
 import React from 'react'
 
 const UploadImage = () => {
 
 
-console.log("UploadImage component");
+    console.log("UploadImage component");
+    
+    // const uploadImage = async() => {
+    //     // 在这里可以处理登录逻辑，使用 username 和 password 的值
+    //     console.log("Username:", username);
+    //     console.log("Password:", password);
+    //     const loginInfo = {
+    //       username: username,
+    //       password: password
+    //     }
+    
+    //     try{
+    //       const response = await api.post("/api/v1/login", loginInfo);
+    //       console.log(response.data)
+    //       if(response.data == true) {
+    //         console.log("Login success!Redirecting to upload image page");
+    //         navigate("/uploadimage");
+    //       }
+    //     } catch(err) {
+    //       console.log(err);
+    //     }
+    //   };
 
 
-
-
+      const [image, setImage] = useState("");
+      const inputFile = useRef(null);
+    
+      const uploadImage = e => {
+        const { files } = e.target;
+        if (files && files.length) {
+          const filename = files[0].name;
+    
+          var parts = filename.split(".");
+          const fileType = parts[parts.length - 1];
+          console.log("fileType", fileType); //ex: zip, rar, jpg, svg etc.
+    
+          setImage(files[0]);
+        }
+      };
+    
+      const onButtonClick = () => {
+        inputFile.current.click();
+      };
+    
+      console.log("imageimage", image);
 
 
 
@@ -20,7 +63,19 @@ console.log("UploadImage component");
 
 
   return (
-    <div>UploadImage</div>
+    <div>
+      <input
+        style={{ display: "none" }}
+        // accept=".zip,.rar"
+        ref={inputFile}
+        onChange={uploadImage}
+        type="file"
+      />
+      <div className="button" onClick={onButtonClick}>
+        Upload
+      </div>
+    </div>
+    
   )
 }
 
