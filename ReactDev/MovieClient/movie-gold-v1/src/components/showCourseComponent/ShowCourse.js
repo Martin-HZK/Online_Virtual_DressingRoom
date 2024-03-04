f48c0e7d5ed891fae74b111c5fd2d8ff7dfe9967
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-03-04 14:32:34 +0800

    Create usercart and showcourse

---
+++ ReactDev/MovieClient/movie-gold-v1/src/components/showCourseComponent/ShowCourse.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './ShowCourse.css'
+const ShowCourse = ({ courses, 
+    filterCourseFunction, 
+    addCourseToCartFunction }) => {
+  return (
+    <div className="product-list">
+            {filterCourseFunction.length === 0 ? (
+                <p className="no-results">
+                    Sorry Geek, No matching Product found.
+                </p>
+            ) : (
+                filterCourseFunction.map((product) => (
+                    <div className="product" key={product.id}>
+                        <img src={product.image} alt={product.name} />
+                        <h2>{product.name}</h2>
+                        <p>Price: ₹{product.price}</p>
+                        <button
+                            className="add-to-cart-button"
+                            onClick={() => addCourseToCartFunction(product)}
+                        >
+                            Add to Shopping Cart
+                        </button>
+                    </div>
+                ))
+            )}
+        </div>
+  )
+}
+
+export default ShowCourse
--- ReactDev/MovieClient/movie-gold-v1/src/pages/dressingRoom/DressingRoom.js
+++ ReactDev/MovieClient/movie-gold-v1/src/pages/dressingRoom/DressingRoom.js
@@ -1,13 +1,98 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './DressingRoom.css'
 import SearchBar from '../../components/searchBar/SearchBar'
+import UserCart from '../../components/userCart/UserCart'
+import ShowCourse from '../../components/showCourseComponent/ShowCourse'
 
 const DressingRoom = () => {
+
+  // these are the tests image
+  //TODO: link the images from the database
+  const [courses, setCourses] = useState([
+    { id: 1, 
+      name: 'GFG T-shirt', 
+      price: 499, 
+      image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165506/gfg1.png'
+    },
+    { id: 2, 
+      name: 'GFG Bag', 
+      price: 699, 
+      image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165553/gfg2.jpg'
+    },
+    { id: 3, 
+      name: 'GFG Hoodie', 
+      price: 799, 
+      image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165623/gfg3.jpg'
+    }
+  ]);
+
+  const [cartCourses, setCartCourses] = useState([]);
+  const [searchCourse, setSearchCourse] = useState('');
+
+
+  const addCourseToCartFunction = (GFGcourse) => {
+    const alreadyCourses = cartCourses
+                           .find(item => item.product.id === GFGcourse.id);
+    if (alreadyCourses) {
+        const latestCartUpdate = cartCourses.map(item =>
+            item.product.id === GFGcourse.id ? { 
+            ...item, quantity: item.quantity + 1 } 
+            : item
+        );
+        setCartCourses(latestCartUpdate);
+    } else {
+        setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+    }
+};
+
+  const deleteCourseFromCartFunction = (GFGCourse) => {
+      const updatedCart = cartCourses
+                          .filter(item => item.product.id !== GFGCourse.id);
+      setCartCourses(updatedCart);
+  };
+
+  const totalAmountCalculationFunction = () => {
+      return cartCourses
+            .reduce((total, item) => 
+                        total + item.product.price * item.quantity, 0);
+  };
+
+  const courseSearchUserFunction = (event) => {
+      setSearchCourse(event.target.value);
+  };
+
+  const filterCourseFunction = courses.filter((course) =>
+      course.name.toLowerCase().includes(searchCourse.toLowerCase())
+  );
+
+
+
+
+
+
+
+
   return (
         
     <div className='dressingroom_container'>
       <div className='items_container'>
         <SearchBar/>
+        <main className="App-main">
+                <ShowCourse
+                    courses={courses}
+                    filterCourseFunction={filterCourseFunction}
+                    addCourseToCartFunction={addCourseToCartFunction}
+                />
+ 
+                <UserCart
+                    cartCourses={cartCourses}
+                    deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+                    totalAmountCalculationFunction={
+                        totalAmountCalculationFunction
+                    }
+                    setCartCourses={setCartCourses}
+                />
+            </main>
         </div>
 
       <div className='model_container'>This is model</div>
