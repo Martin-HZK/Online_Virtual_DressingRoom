648a17790b4a53c03c3a11229d7ce67c44433998
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-03-16 21:52:24 +0800

    Create the show goods component for retailer mode

--- ReactDev/MovieClient/movie-gold-v1/src/pages/retailer_pages/upload_goods/UploadGoods.js
+++ ReactDev/MovieClient/movie-gold-v1/src/pages/retailer_pages/upload_goods/UploadGoods.js
@@ -5,6 +5,7 @@ import './UploadGoods.css'
 import 'rsuite/dist/rsuite.min.css'
 import { Button, Modal } from "rsuite";
 import RetailerShowGoods from '../../../components/retailerShowGoods/RetailerShowGoods';
+import ShowCourse from '../../../components/showCourseComponent/ShowCourse';
 
 const UploadGoods = () => {
   
@@ -64,19 +65,114 @@ const UploadGoods = () => {
     const handleOpen = () => setOpen(true); 
     const handleClose = () => setOpen(false);
 
-    const [courses, setCourses] = useState([]);
+    // const [courses, setCourses] = useState([]);
 
     // Here we need to communicate with the 
+    const [courses, setCourses] = useState([
+        { id: 1, 
+          name: 'GFG T-shirt', 
+          price: 499, 
+          image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165506/gfg1.png'
+        },
+        { id: 2, 
+          name: 'GFG Bag', 
+          price: 699, 
+          image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165553/gfg2.jpg'
+        },
+        { id: 3, 
+          name: 'GFG Hoodie', 
+          price: 799, 
+          image: 'https://media.geeksforgeeks.org/wp-content/uploads/20230823165623/gfg3.jpg'
+        },
+        { id: 4, 
+          name: 'Test clothes', 
+          price: 799, 
+          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+        },
+        {
+        name: 'Test clothes', 
+        price: 799, 
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+        },
+        {
+          name: 'Test clothes', 
+          price: 799, 
+          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+          },
+          {
+            name: 'Test clothes', 
+            price: 799, 
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+            },
+            {
+              name: 'Test clothes', 
+              price: 799, 
+              image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+              },
+              {
+                name: 'Test clothes', 
+                price: 799, 
+                image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPren7APkmhMhZHhpgoXocDcDvhsiIqO4iog&usqp=CAU'
+                }
+      ]);
 
+    
 
+    const [cartCourses, setCartCourses] = useState([]);
+    const [searchCourse, setSearchCourse] = useState('');
+
+    const filterCourseFunction = courses.filter((course) =>
+    course.name.toLowerCase().includes(searchCourse.toLowerCase())
+  );
+    // this will not be used here
+    const addCourseToCartFunction = (GFGcourse) => {
+        const alreadyCourses = cartCourses
+                               .find(item => item.product.id === GFGcourse.id);
+        if (alreadyCourses) {
+            const latestCartUpdate = cartCourses.map(item =>
+                item.product.id === GFGcourse.id ? { 
+                ...item, quantity: item.quantity + 1 } 
+                : item
+            );
+            setCartCourses(latestCartUpdate);
+        } else {
+            setCartCourses([...cartCourses, {product: GFGcourse, quantity: 1}]);
+        }
+    };
+
+    const deleteCourseFromCartFunction = (GFGCourse) => {
+        const updatedCart = cartCourses
+                            .filter(item => item.product.id !== GFGCourse.id);
+        setCartCourses(updatedCart);
+    };
+  
+    const totalAmountCalculationFunction = () => {
+        return cartCourses
+              .reduce((total, item) => 
+                          total + item.product.price * item.quantity, 0);
+    };
+    const courseSearchUserFunction = (event) => {
+        setSearchCourse(event.target.value);
+    };
+  
 
     
   
   return (
       <div className='upload-container'>
-        
-            <RetailerShowGoods
-                courses={courses}/>
+            <div className='show-goods-container'>
+                <RetailerShowGoods
+                    courses={courses}
+                    filterCourseFunction={filterCourseFunction}
+                    addCourseToCartFunction={addCourseToCartFunction}
+                />
+            </div>
+
+            {/* <ShowCourse
+                courses={courses}
+                filterCourseFunction={filterCourseFunction}
+                addCourseToCartFunction={addCourseToCartFunction}
+            /> */}
 
             <div className='upload-goods-container' > 
                     {/* <span>Modal with Overflow </span>  */}
@@ -87,16 +183,28 @@ const UploadGoods = () => {
                     <Modal overflow={overflow} 
                          open={open} onClose={handleClose}> 
                         <Modal.Header> 
-                            <Modal.Title>GeeksforGeeks</Modal.Title> 
+                            <Modal.Title>Upload Your Goods!</Modal.Title> 
                         </Modal.Header> 
-                        <Modal.Body className='modal-body'> 
-                          hi this is the user body
-                          <input
+                        <Modal.Body className='modal-body'>
+                          {/* <input
                                 name="file"
                                 type="file"
+                                style={{ display: "none" }}// accept=".zip,.rar"
+                                onChange={onFileChange}
+                            /> */}
+                            
+                            <input
+                                id="fileInput"
+                                name="file"
+                                type="file"
+                                style={{ display: "none" }}
+                                accept=".zip,.rar"
                                 onChange={onFileChange}
                             />
-                            <button onClick={onFileUpload}>Upload</button>
+                            <button onClick={() => document.getElementById('fileInput').click()}>Select File</button>
+                         
+                         <div className='chunck'>
+                        <label>Goods Name</label>
 
                             <input 
                                 type="text" 
@@ -105,26 +213,43 @@ const UploadGoods = () => {
                                 onChange={handleGoodsNameChange} // 当输入框的值发生变化时调用 handleChange 函数
                             />
 
+                        </div>
+
+                        <div className='chunck'>
+                        <label>Description</label>
                             <input
                                 type="text"
                                 placeholder="Description"
                                 value={description}
                                 onChange={handleGoodsDescriptionChange}
                             />
+                        
+                        </div>
 
+                        <div className='chunk'>
+                        <label>Category</label>
                             <input
                                 type="text"
                                 placeholder="Category"
                                 value={category}
                                 onChange={handleGoodsCategoryChange}
                             />
-
+                            
+                        </div>
+                            
+                        <div className='chunk'>
+                        <label>Brand</label>
                             <input
                                 type='text'
                                 placeholder='Brand'
                                 value={brand}
                                 onChange={handleGoodsBrandChange}
                             />
+                        
+                        </div>
+
+                            <button onClick={onFileUpload}>Upload</button>
+
                         </Modal.Body> 
                         <Modal.Footer> 
                             <Button onClick={handleClose} appearance="primary"> 
