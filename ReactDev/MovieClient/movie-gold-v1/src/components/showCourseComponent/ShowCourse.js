3493a5c3357d21961d16ca06b982d3b8ac1a38b2
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-03-17 16:16:27 +0800

    Add the function of clicking to show big pics

--- ReactDev/MovieClient/movie-gold-v1/src/components/showCourseComponent/ShowCourse.js
+++ ReactDev/MovieClient/movie-gold-v1/src/components/showCourseComponent/ShowCourse.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './ShowCourse.css'
+import { useState } from 'react';
+import Modal from 'react-modal';
+// import Modal from "rsuite";
+// import 'rsuite/dist/rsuite.min.css'
+import { set } from 'rsuite/esm/utils/dateUtils';
+
 const ShowCourse = ({ courses, 
     filterCourseFunction, 
     addCourseToCartFunction }) => {
-  return (
-    <div className="product-list">
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const openModal = (product) => {
+        console.log(product);
+        setSelectedImage(product);
+        setModalIsOpen(true);
+    }
+    const closeModal = () => {
+        setSelectedImage(null);
+        setModalIsOpen(false);
+    }
+    const handleReturnButtonClick = () => {
+        closeModal(); // 关闭弹窗
+        // 在这里添加返回的逻辑，比如返回上一页或者其他操作
+      };
+    return (
+        <div className="product-list">
             {filterCourseFunction.length === 0 ? (
                 <p className="no-results">
                     Sorry Geek, No matching Product found.
@@ -12,7 +34,10 @@ const ShowCourse = ({ courses,
             ) : (
                 filterCourseFunction.map((product) => (
                     <div className="product" key={product.id}>
-                        <img src={product.image} alt={product.name} />
+                        <img src={product.image} 
+                        alt={product.name} 
+                        onClick={() => openModal(product.image)}
+                        />
                         <h2>{product.name}</h2>
                         <p>Price: ₹{product.price}</p>
                         <button
@@ -24,7 +49,19 @@ const ShowCourse = ({ courses,
                     </div>
                 ))
             )}
-        </div>
+
+        <Modal className="custom-modal" isOpen={modalIsOpen} onRequestClose={closeModal}>
+            <img
+            src={selectedImage}
+            alt="Large Image"
+            onClick={closeModal}
+            />
+            <div className="footer-container">
+            {/* 返回按钮 */}
+                <button onClick={handleReturnButtonClick}>Close</button>
+            </div>
+        </Modal>
+    </div>
   )
 }
 
