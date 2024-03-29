57389078ff9a3089270a33fd8f5f437f4dfce260
Martin-HZK <ManUnitedHZK7@outlook.com>
2024-03-29 14:10:49 +0800

    Enable caching the userinfo while refreshing the page

--- ReactDev/MovieClient/movie-gold-v1/src/userContextProvider/UserContextProvider.js
+++ ReactDev/MovieClient/movie-gold-v1/src/userContextProvider/UserContextProvider.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { createContext } from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const UserContext = createContext();
 
-const UserContextProvider = ({children}) => {
-    const [globUsername, setGlobUsername] = useState('');
+// const UserContextProvider = ({children}) => {
+//     const [globUsername, setGlobUsername] = useState('');
+
+//   return (
+//     <UserContext.Provider value={{globUsername, setGlobUsername}}>
+//       {children}
+//     </UserContext.Provider>
+//   )
+// }
+const UserContextProvider = ({ children }) => {
+  const [globUsername, setGlobUsername] = useState(() => {
+    const savedGlobUsername = localStorage.getItem('globUsername');
+    return savedGlobUsername || ""; // 如果localStorage中没有，则默认为空字符串
+  });
+
+  useEffect(() => {
+    // 每当 globUsername 更新时，将其保存到 localStorage
+    localStorage.setItem('globUsername', globUsername);
+  }, [globUsername]);
 
   return (
-    <UserContext.Provider value={{globUsername, setGlobUsername}}>
+    <UserContext.Provider value={{ globUsername, setGlobUsername }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
 export { UserContextProvider, UserContext }
