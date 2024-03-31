2f2213facfeb321f9427451eca7b83785c3be26f
ShuaijieChen <2430727258@qq.com>
2024-03-31 17:27:00 +0800

    Beutify image slider

--- ReactDev/MovieClient/movie-gold-v1/src/components/imageSlider/ImageSlider.js
+++ ReactDev/MovieClient/movie-gold-v1/src/components/imageSlider/ImageSlider.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import './ImageSlider.css'; // 确保你创建了一个对应的CSS文件
-
+import './ImageSlider.css'; 
 const ImageSlider = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -16,13 +15,32 @@ const ImageSlider = ({ images }) => {
     setCurrentIndex(newIndex);
   };
 
+  const [Nimages, setNImages] = useState([
+    'Images/1_768x1024_08015_00.png',
+    'Images/1_768x1024_08015_00.png',
+    'Images/1_768x1024_11001_00.png',
+    'Images/1_768x1024_13973_00.png',  
+    'Images/1_768x1024_13987_00.png',
+    'Images/1_768x1024_14096_00.png'
+    
+  ]);
+
+
   return (
-    <div className="slider">
-      <button onClick={goToPrevious}>&lt;</button>
-      <img src={images[currentIndex]} alt="slider-img" />
-      <button onClick={goToNext}>&gt;</button>
-    </div>
-  );
+    // <div className="slider">
+    //   <button onClick={goToPrevious}>&lt;</button>
+    //   {/* <img src={images[currentIndex]} alt="slider-img" /> */}
+    // <img src={Nimages[currentIndex]} alt="slider-img" />
+    //   <button onClick={goToNext}>&gt;</button>
+    // </div>
+      <div className="slider">
+        <img src={Nimages[currentIndex]} alt="slider-img" />
+        <div className="slider-controls">
+          <button onClick={goToPrevious}>&lt;</button>
+          <button onClick={goToNext}>&gt;</button>
+        </div>
+      </div>
+    );
 };
 
 export default ImageSlider;
